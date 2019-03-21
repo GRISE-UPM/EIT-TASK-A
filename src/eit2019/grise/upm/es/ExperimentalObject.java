80670d7f9fc8c8d072bd4eb2cd41a7d03c6d337e
Oscar Dieste <odieste@fi.upm.es>
2019-03-21 10:07:50 +0100

    Project stub

---
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -0,0 +1,22 @@
+package eit2019.grise.upm.es;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExperimentalObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		// TODO Method stub
+		
+		return list;
+	}
+
+}
