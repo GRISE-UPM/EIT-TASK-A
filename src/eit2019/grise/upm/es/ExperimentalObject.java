f350bb2cfcd3cb7d2a4d573adec9609e91f34cb4
coco10@ccfi.fi.upm.es <coco10@ccfi.fi.upm.es>
2019-03-25 15:44:16 +0100

    Good

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class ExperimentalObject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			System.out.println(ExperimentalObject.getPrimeFactorDecomposition(4));		
+			System.out.println(ExperimentalObject.getPrimeFactorDecomposition(6));
+			System.out.println(ExperimentalObject.getPrimeFactorDecomposition(15));
+		} catch (NegativeNumbersNotAllowed e) {
+			e.printStackTrace();
+		}
 
 	}
 	
@@ -14,8 +20,19 @@ public class ExperimentalObject {
 		
 		List<Integer> list = new ArrayList<Integer>();
 		
-		// TODO Method stub
+		boolean stillDivisible = true;
 		
+		int divisible = number;
+			for(int i = 2; i < number; i++) {
+				if(divisible%i == 0) {
+					list.add(i);
+					divisible = divisible/i;
+				}
+			}
+		if(divisible > 1) {
+			list.add(divisible);
+		}
+			
 		return list;
 	}
 
