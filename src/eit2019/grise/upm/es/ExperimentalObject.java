217b577c2893adbd30bebc023651e1c90f7ebc06
R-Dog <37508579+Roger-G@users.noreply.github.com>
2019-03-25 15:57:47 +0100

    Update ExperimentalObject.java

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -13,7 +13,14 @@ public class ExperimentalObject {
 	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
 		
 		List<Integer> list = new ArrayList<Integer>();
-		
+		for (int i=1;;i++) {
+			if (number/i==0) {
+				list.add(i);
+				if(i!=number/i) {
+					list.add(number/i);
+				}
+			}
+		}
 		// TODO Method stub
 		
 		return list;
