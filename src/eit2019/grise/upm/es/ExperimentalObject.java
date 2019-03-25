b904d62ae7084c2083ec284e79c557bc7204c388
Lukas Bittner <4bittner@informatik.uni-hamburg.de>
2019-03-25 15:40:31 +0100

    Prime factor decomposistion implemented

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -3,20 +3,37 @@ package eit2019.grise.upm.es;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
+
 public class ExperimentalObject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 12;
+		try {
+			List<Integer> factors = getPrimeFactorDecomposition(i);
+			System.out.println(factors);
+		} catch (NegativeNumbersNotAllowed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 	
 	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
 		
-		List<Integer> list = new ArrayList<Integer>();
 		
-		// TODO Method stub
-		
-		return list;
+		int n = number;
+        List<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return factors;
+
 	}
 
 }
