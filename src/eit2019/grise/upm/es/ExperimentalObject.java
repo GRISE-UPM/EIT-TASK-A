52dc95904e742f1e2630ddb7e75721acebcf5141
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2019-03-25 16:29:50 +0100

    my results
    my results

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -1,21 +1,38 @@
 package eit2019.grise.upm.es;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExperimentalObject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NegativeNumbersNotAllowed {
+		List<Integer> list = new ArrayList<Integer>();
+		list = getPrimeFactorDecomposition(49);
+		System.out.println("My list for 49 = "+Arrays.toString(list.toArray()));
+		list = getPrimeFactorDecomposition(5);
+		System.out.println("My list for 5 = "+Arrays.toString(list.toArray()));
+		list = getPrimeFactorDecomposition(64);
+		System.out.println("My list for 64 = "+Arrays.toString(list.toArray()));
+		list = getPrimeFactorDecomposition(99);
+		System.out.println("My list for 99 = "+Arrays.toString(list.toArray()));
+		list = getPrimeFactorDecomposition(199);
+		System.out.println("My list for 199 = "+Arrays.toString(list.toArray()));
 	}
 	
 	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
 		
 		List<Integer> list = new ArrayList<Integer>();
-		
-		// TODO Method stub
-		
+
+		while (number>1) {
+			for(int j = 2; j<=number; j++) {
+				if(number%j == 0) {
+					list.add(j);
+					number = number/j;
+					j=number;
+				}
+			}
+		}
 		return list;
 	}
 
