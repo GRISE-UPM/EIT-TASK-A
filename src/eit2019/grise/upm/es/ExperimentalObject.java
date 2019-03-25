5f37c0baabc91c43c418d72bcd3b82f7fb1b019d
Yerai <yerai@users.noreply.github.com>
2019-03-25 15:51:18 +0100

    Update ExperimentalObject.java

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -5,16 +5,35 @@ import java.util.List;
 
 public class ExperimentalObject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NegativeNumbersNotAllowed {
+		//Number to be factorized
+		Integer number = 12;
+		
+		// Call exception
+		if(number <= 0) {
+			throw new NegativeNumbersNotAllowed ("Number should not be lower than zero");
+		}
+		
+		//Call function
+		List<Integer> factorList = getPrimeFactorDecomposition(number);
+		
+		//Print results
+		System.out.println(factorList.toString());
 	}
 	
 	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
 		
+		//Create list of factors
 		List<Integer> list = new ArrayList<Integer>();
 		
-		// TODO Method stub
+		for(int factor=2; factor<=number; ) {
+			if(number%factor == 0) { // If the number divided by factor has no decimals
+				list.add(factor); //Add the factor to the list
+				number = number/factor; // Divide the number by the factor
+			}else {
+				factor++; //If the factor is not useful, add ones
+			}
+		}
 		
 		return list;
 	}
