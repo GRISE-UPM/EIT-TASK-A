8ae1faae6aded82cbd69f71eab741fe0c5499ff6
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2019-04-07 20:34:23 +0200

    update icon
    update icon

--- src/eit2019/grise/upm/es/ExperimentalObject.java
+++ src/eit2019/grise/upm/es/ExperimentalObject.java
@@ -10,14 +10,26 @@ public class ExperimentalObject {
 		List<Integer> list = new ArrayList<Integer>();
 		list = getPrimeFactorDecomposition(49);
 		System.out.println("My list for 49 = "+Arrays.toString(list.toArray()));
+		
 		list = getPrimeFactorDecomposition(5);
 		System.out.println("My list for 5 = "+Arrays.toString(list.toArray()));
+		
 		list = getPrimeFactorDecomposition(64);
 		System.out.println("My list for 64 = "+Arrays.toString(list.toArray()));
+		
+		list = getPrimeFactorDecomposition(87);
+		System.out.println("My list for 87 = "+Arrays.toString(list.toArray()));
+	
 		list = getPrimeFactorDecomposition(99);
 		System.out.println("My list for 99 = "+Arrays.toString(list.toArray()));
+		
 		list = getPrimeFactorDecomposition(199);
 		System.out.println("My list for 199 = "+Arrays.toString(list.toArray()));
+		
+		list = getPrimeFactorDecomposition(3863);
+		System.out.println("My list for 3863 = "+Arrays.toString(list.toArray()));
+		
+		
 	}
 	
 	public static List<Integer> getPrimeFactorDecomposition(int number) throws NegativeNumbersNotAllowed {
